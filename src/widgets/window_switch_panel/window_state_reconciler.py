631fb35f9ddbd60f591754f75502db165dbec863
darco-ctrl <darcowb388@gmail.com>
2026-07-25 15:15:32 +0530

    made StateReconciler

--- src/widgets/window_switch_panel/window_state_reconciler.py
+++ src/widgets/window_switch_panel/window_state_reconciler.py
@@ -1,3 +1,52 @@
+from dataclasses import dataclass
+
+from widgets.window_switch_panel.window_item import WindowItem
+from windows.window import WindowInfo
+from windows.window_scanner import WindowScanner
+
+
+@dataclass
+class TaskList:
+    update: list[int]
+    new: list[WindowInfo]
+    delete: list[int]
+
+
 class StateReconciler:
-    def __init__():
-        pass
+    def __init__(self, window_scanner: WindowScanner):
+        self.window_scanner = window_scanner
+
+    def update_window_items(self, window_item_lists: list[WindowItem]) -> TaskList:
+
+        open_windows: list[WindowInfo] = self.window_scanner.get_windows_info()
+
+        update_window: list[int] = []
+        new_window: list[WindowInfo] = []
+        delete_window: list[int] = []
+
+        for window in open_windows:
+            match = next(
+                (
+                    (index, item)
+                    for index, item in enumerate(window_item_lists)
+                    if item.hwnd == window.hwnd
+                ),
+                None,
+            )
+
+            if match:
+                index, item = match
+                update_window.append(index)
+
+            else:
+                new_window.append(window)
+
+        for i in range(0, len(window_item_lists)):
+            if not i in update_window:
+                delete_window.append(i)
+
+        task_list: TaskList = TaskList(
+            delete=delete_window, new=new_window, update=update_window
+        )
+
+        return task_list
