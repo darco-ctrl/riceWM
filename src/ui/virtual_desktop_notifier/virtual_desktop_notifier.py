967d3150a6a5bb1a089bdc0a6ec0b872beda7ecb
darco-ctrl <darcowb388@gmail.com>
2026-09-02 18:26:55 +0530

    Created File and Folder structure for VirtualDesktopNotifier

---
+++ src/ui/virtual_desktop_notifier/virtual_desktop_notifier.py
@@ -0,0 +1,14 @@
+
+
+from PySide6.QtWidgets import QWidget
+
+
+class VirtualDesktopNotifier(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def load(self):
+        pass
+
+    def reload(self):
+        pass
